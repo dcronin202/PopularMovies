11cbf6ae8ca4b59df73bac3b09ac0c7075b5d48c
Daniel Cronin <dcronin202@gmail.com>
2019-08-01 08:11:07 -0500

    Created a JsonUtils class

---
+++ app/src/main/java/com/example/android/popularmovies_v1/data/JsonUtils.java
@@ -0,0 +1,16 @@
+package com.example.android.popularmovies_v1.data;
+
+import org.json.JSONObject;
+
+public class JsonUtils {
+
+    // Tag for log messages
+    private static final String LOG_TAG = JsonUtils.class.getSimpleName();
+
+    // TODO: REMOVE API KEY BEFORE SUBMITTING
+    private static final String apiKey = "301ade02190b07969335c116b1456868";
+
+    private static final String MOVIE_DB_URL = "http://api.themoviedb.org/3/movie/popular?api_key=" + apiKey;
+
+
+}
