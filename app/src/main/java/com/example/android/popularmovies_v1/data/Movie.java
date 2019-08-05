d77cb4d86be1d629540dbcc798f92ec6a4bbe19b
Daniel Cronin <dcronin202@gmail.com>
2019-08-05 15:09:38 -0500

    Added code for Parcelables

--- app/src/main/java/com/example/android/popularmovies_v1/data/Movie.java
+++ app/src/main/java/com/example/android/popularmovies_v1/data/Movie.java
@@ -1,6 +1,11 @@
 package com.example.android.popularmovies_v1.data;
 
-public class Movie {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.List;
+
+public class Movie implements Parcelable {
 
     private String movieTitle;
     private String originalTitle;
@@ -9,7 +14,6 @@ public class Movie {
     private double rating;
     private String releaseDate;
     private double popularity;
-
 
     public Movie() {
         }
@@ -79,5 +83,45 @@ public class Movie {
     public void setPopularity() {
         this.popularity = popularity;
     }
+
+    // Code for Parcels
+    private Movie(Parcel p) {
+        movieTitle = p.readString();
+        originalTitle = p.readString();
+        posterImage = p.readString();
+        plot = p.readString();
+        rating = p.readDouble();
+        releaseDate = p.readString();
+        popularity = p.readDouble();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(movieTitle);
+        parcel.writeString(originalTitle);
+        parcel.writeString(posterImage);
+        parcel.writeString(plot);
+        parcel.writeDouble(rating);
+        parcel.writeString(releaseDate);
+        parcel.writeDouble(popularity);
+
+    }
+
+    public final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel parcel) {
+            return new Movie(parcel);
+        }
+
+        @Override
+        public Movie[] newArray(int i) {
+            return new Movie[i];
+        }
+    };
 
 }
