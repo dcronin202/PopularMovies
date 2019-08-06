7dce44b65e5bf9007be8279b8bbbae371f6ce17b
Daniel Cronin <dcronin202@gmail.com>
2019-08-06 10:32:27 -0500

    Updated intent and reformatted the displayed release date and average rating

--- app/src/main/java/com/example/android/popularmovies_v1/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies_v1/DetailActivity.java
@@ -1,32 +1,50 @@
 package com.example.android.popularmovies_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.popularmovies_v1.data.Movie;
 import com.squareup.picasso.Picasso;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class DetailActivity extends AppCompatActivity {
 
-    public static final String EXTRA_POSITION = "extra position";
+    public static final String MOVIE_DETAILS = "movie";
 
-    private static final int DEFAULT_POSITION = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // dummy data
-        Movie testMovie = new Movie("The Lion King", "The Lion King","https://image.tmdb.org/t/p/w185//dzBtMocZuJbjLOXvrl4zGYigDzh.jpg", "Simba idolises his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub's arrival. Scar, Mufasa's brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba's exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.", 7.2, "2019-07-12", 425.026);
 
-        populateMovieDetails(testMovie);
+        Intent intent = getIntent();
 
-        // Change DetailActivity title/header to match movie title
-        setTitle(testMovie.getMovieTitle());
+        Movie movieDetails = intent.getParcelableExtra(MOVIE_DETAILS);
 
+        if (movieDetails != null) {
+
+            populateMovieDetails(movieDetails);
+            // Change DetailActivity title/header to match movie title
+            setTitle(movieDetails.getMovieTitle());
+
+        } else {
+            closeOnError();
+        }
+    }
+
+    private void closeOnError() {
+        Toast.makeText(this, R.string.error_toast_message, Toast.LENGTH_LONG).show();
+        finish();
     }
 
     private void populateMovieDetails(Movie movie) {
@@ -45,13 +63,27 @@ public class DetailActivity extends AppCompatActivity {
         TextView originalTitle = findViewById(R.id.original_title);
         originalTitle.setText(movie.getOriginalTitle());
 
-        // Year
+        // Year - reformatted original String input to display MMMM yyyy format
+        String releaseDate = movie.getReleaseDate();
+        DateFormat oldDateFormat = new SimpleDateFormat("yyyy-MM-DD", Locale.ENGLISH);
+        Date newReleaseDate = null;
+        try {
+            newReleaseDate = (Date) oldDateFormat.parse(releaseDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat newDateFormat = new SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
+        String finalDate = newDateFormat.format(newReleaseDate);
+
         TextView yearReleased = findViewById(R.id.release_date);
-        yearReleased.setText(movie.getReleaseDate());
+        yearReleased.setText(finalDate);
 
         // Avg Rating
+        String movieRating = String.valueOf(movie.getRating());
+        String avgMovieRating = movieRating + getString(R.string.out_of_ten);
+
         TextView averageRating = findViewById(R.id.average_rating);
-        averageRating.setText(movie.getRating() + "/10");  // TODO: Once this is working, convert the hard-coded String to a resource string
+        averageRating.setText(avgMovieRating);
 
         // Plot Summary
         TextView plotSummary = findViewById(R.id.plot_summary);
