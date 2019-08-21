bcef569af57f60d0e3159f1a1b0b27dc661847ed
Daniel Cronin <dcronin202@gmail.com>
2019-08-21 15:05:06 -0500

    Updates to get overview data displayed in the movie_overview_fragment TextView

--- app/src/main/java/com/example/android/popularmovies/fragment/MovieOverviewFragment.java
+++ app/src/main/java/com/example/android/popularmovies/fragment/MovieOverviewFragment.java
@@ -1,22 +1,31 @@
 package com.example.android.popularmovies.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.android.popularmovies.data.Movie;
 import com.example.android.popularmovies.R;
 
 public class MovieOverviewFragment extends Fragment {
 
     private static final String TAG = "MovieOverviewFragment";
 
+    private TextView overview;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.movie_overview_fragment, container, false);
+
+        Movie movie = new Movie();
+
+        overview = view.findViewById(R.id.overview);
+        overview.setText(movie.getPlot());
 
         return view;
 
