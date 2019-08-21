d423be3c4c294bfe4f9fd07881688adf7b33f72b
Daniel Cronin <dcronin202@gmail.com>
2019-08-21 09:06:00 -0500

    Added call for getMovies()

---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieResponse.java
@@ -0,0 +1,20 @@
+package com.example.android.popularmovies.data;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieResponse {
+
+    @SerializedName("results")
+    public List<Movie> movieResults;
+
+    public List<Movie> getMovieResults() {
+        return movieResults;
+    }
+
+    public void setMovieResults(List<Movie> movieResults) {
+        this.movieResults = movieResults;
+    }
+
+}
