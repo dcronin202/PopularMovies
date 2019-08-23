4d4f609b063984ec2d1fa8d0d0f22c474849e492
Daniel Cronin <dcronin202@gmail.com>
2019-08-23 08:34:37 -0500

    Added code for Parcels

--- app/src/main/java/com/example/android/popularmovies/data/MovieVideos.java
+++ app/src/main/java/com/example/android/popularmovies/data/MovieVideos.java
@@ -1,8 +1,11 @@
 package com.example.android.popularmovies.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class MovieVideos {
+public class MovieVideos implements Parcelable {
 
     @SerializedName("name")
     private String videoName;
@@ -25,8 +28,46 @@ public class MovieVideos {
         return youTubeVideoPath + videoUrlKey;
     }
 
-    public void setMovieVideos(String movieVideo) {
+    public void setMovieVideos(String videoUrlKey) {
         this.videoUrlKey = videoUrlKey;
     }
+
+    public MovieVideos() {
+    }
+
+    public MovieVideos(String videoName, String videoUrlKey) {
+        this.videoName = videoName;
+        this.videoUrlKey = videoUrlKey;
+    }
+
+
+    // Code for Parcels
+    private MovieVideos(Parcel p) {
+        videoName = p.readString();
+        videoUrlKey = p.readString();
+    }
+
+    @Override
+    public int describeContents(){
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(videoName);
+        parcel.writeString(videoUrlKey);
+    }
+
+    public final static Parcelable.Creator<MovieVideos> CREATOR = new Parcelable.Creator<MovieVideos>() {
+        @Override
+        public MovieVideos createFromParcel(Parcel parcel) {
+            return new MovieVideos(parcel);
+        }
+
+        @Override
+        public MovieVideos[] newArray(int i) {
+            return new MovieVideos[i];
+        }
+    };
 
 }
