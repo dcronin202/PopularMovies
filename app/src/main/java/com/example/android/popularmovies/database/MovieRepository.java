3f8e4299d9cb526ab12cce40e550d22b1650558a
Daniel Cronin <dcronin202@gmail.com>
2019-08-28 10:33:24 -0500

    Moved Retrofit movie calls from the MainActivityFragment to the MovieRepository

--- app/src/main/java/com/example/android/popularmovies/database/MovieRepository.java
+++ app/src/main/java/com/example/android/popularmovies/database/MovieRepository.java
@@ -2,49 +2,120 @@ package com.example.android.popularmovies.database;
 
 import android.app.Application;
 import android.arch.lifecycle.LiveData;
-import android.os.AsyncTask;
+import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.TextView;
 
+import com.example.android.popularmovies.adapter.MovieAdapter;
+import com.example.android.popularmovies.data.JsonMovieApi;
 import com.example.android.popularmovies.data.Movie;
+import com.example.android.popularmovies.data.MovieResponse;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MovieRepository {
 
-    private MovieDao mMovieDao;
-    private LiveData<List<Movie>> mAllMovies;
+    private static final String LOG_TAG = MovieRepository.class.getSimpleName();
+    // TODO: API KEY GOES HERE
+    private static final String apiKey = " ";
+    private static final String MOVIE_URL = "http://api.themoviedb.org/3/movie/";
+    private JsonMovieApi jsonMovieApi;
+
+    private MovieDao movieDao;
+    private MutableLiveData<List<Movie>> movies;
 
     MovieRepository(Application application) {
         MovieFavoritesDatabase database = MovieFavoritesDatabase.getInstance(application);
-        mMovieDao = database.movieDao();
-        mAllMovies = mMovieDao.loadAllMovies();
+        movieDao = database.movieDao();
+        //movies = movieDao.loadAllMovies();
+        movies = new MutableLiveData<>();
 
     }
 
-    LiveData<List<Movie>> getAllMovies() {
-        return mAllMovies;
+    // Need to merge Retrofit with the LiveData getAllMovies()
+    public LiveData<List<Movie>> getAllMovies() {
+        return movies;
     }
 
 
-    // Replace this section w/ Retrofit code
-    public void insert(Movie movie) {
-        new insertAsyncTask(mMovieDao).execute(movie);
+    // RETROFIT Methods //
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(MOVIE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    private void getJsonMovieApi() {
+        jsonMovieApi = retrofit.create(JsonMovieApi.class);
     }
 
-    private static class insertAsyncTask extends AsyncTask<Movie, Void, Void> {
-
-        private MovieDao mAsyncMovieDao;
-
-        insertAsyncTask(MovieDao dao) {
-            mAsyncMovieDao = dao;
+    // Method for retrieving Popular movies
+    public void callPopularMovies() {
+        if (jsonMovieApi == null) {
+            getJsonMovieApi();
         }
 
-        @Override
-        protected Void doInBackground(final Movie... params) {
-            mAsyncMovieDao.insertMovie(params[0]);
-            return null;
+        Call<MovieResponse> call = jsonMovieApi.getPopularMovies(apiKey);
+
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                onMovieResponseReceived(response);
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Log.e(LOG_TAG, t.getMessage());
+            }
+        });
+    }
+
+    // Method for retrieving Top Rated movies
+    public void callTopRatedMovies() {
+        if (jsonMovieApi == null) {
+            getJsonMovieApi();
         }
 
+        Call<MovieResponse> call = jsonMovieApi.getTopRatedMovies(apiKey);
+
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                onMovieResponseReceived(response);
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Log.e(LOG_TAG, t.getMessage());
+            }
+        });
+    }
+
+    // Method for receiving a Movie response object and displaying its data
+    private void onMovieResponseReceived(Response<MovieResponse> response) {
+
+        if (response.isSuccessful()) {
+
+            MovieResponse movieResponse = response.body();
+            List<Movie> movieResults = movieResponse.getMovieResults();
+            movies.postValue(movieResults);
+
+
+        } else {
+            movies.postValue(new ArrayList<Movie>());
+            Log.e(LOG_TAG, "Code: " + response.code());
+
+        }
     }
 
 }
--- app/src/main/java/com/example/android/popularmovies/fragment/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popularmovies/fragment/MainActivityFragment.java
@@ -1,7 +1,11 @@
 package com.example.android.popularmovies.fragment;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,6 +21,8 @@ import com.example.android.popularmovies.R;
 import com.example.android.popularmovies.data.JsonMovieApi;
 import com.example.android.popularmovies.data.Movie;
 import com.example.android.popularmovies.data.MovieResponse;
+import com.example.android.popularmovies.database.MovieFavoritesDatabase;
+import com.example.android.popularmovies.database.MovieViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,44 +43,19 @@ public class MainActivityFragment extends Fragment {
     private MovieAdapter movieAdapter;
 
     private GridView gridView;
-
     private TextView errorMessage;
 
-    private JsonMovieApi jsonMovieApi;
-
-
-    // TODO: API KEY GOES HERE
-    private static final String apiKey = " ";
-
-    // URL for movie data
-    private static final String MOVIE_URL = "http://api.themoviedb.org/3/movie/";
+    private MovieFavoritesDatabase movieDatabase;
+    private MovieViewModel viewModel;
 
 
     public MainActivityFragment() {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(MOVIE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        jsonMovieApi = retrofit.create(JsonMovieApi.class);
-
-        getPopularMovies();
-
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Set up MovieViewModel
+        setupViewModel();
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
@@ -86,7 +67,6 @@ public class MainActivityFragment extends Fragment {
 
         // Get a reference to the error message
         errorMessage = rootView.findViewById(R.id.error_message_display);
-
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -106,77 +86,50 @@ public class MainActivityFragment extends Fragment {
         startActivity(intent);
     }
 
+    private void setupViewModel() {
+        viewModel = ViewModelProviders.of(getActivity()).get(MovieViewModel.class);
+        viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(@Nullable List<Movie> movies) {
+                if (movies.size() > 0) {
+                    // Clear any previous data
+                    movieAdapter.clear();
+
+                    // Add new movie data
+                    Log.d(LOG_TAG, "Updated list of movies from LiveData in ViewModel");
+                    movieAdapter.addAll(movies);
+
+                    gridView.setVisibility(View.VISIBLE);
+                    errorMessage.setVisibility(View.GONE);
+
+                } else {
+                    errorMessage.setVisibility(View.VISIBLE);
+                    gridView.setVisibility(View.GONE);
+
+                }
+            }
+        });
+        viewModel.getPopularMovies();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
     public void sortByPopularity() {
-        getPopularMovies();
+        viewModel.getPopularMovies();
     }
 
     public void sortByTopRated() {
-        getTopRatedMovies();
+        viewModel.getTopRatedMovies();
     }
 
+    /* Need to get this set up once database is working
     public void sortByFavorites() {
-        // getFavoriteMovies();
-    }
+        movieDatabase = MovieFavoritesDatabase.getInstance(getContext()); // should be getApplicationContext instead?
+        setupViewModel();
+    } */
 
-    // Method for retrieving Popular movies
-    private void getPopularMovies() {
-
-        Call<MovieResponse> call = jsonMovieApi.getPopularMovies(apiKey);
-
-        call.enqueue(new Callback<MovieResponse>() {
-
-            @Override
-            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
-                onMovieResponseReceived(response);
-
-            }
-
-            @Override
-            public void onFailure(Call<MovieResponse> call, Throwable t) {
-                Log.e(LOG_TAG, t.getMessage());
-
-            }
-        });
-    }
-
-    // Method for retrieving Top Rated Movies
-    private void getTopRatedMovies() {
-
-        Call<MovieResponse> call = jsonMovieApi.getTopRatedMovies(apiKey);
-
-        call.enqueue(new Callback<MovieResponse>() {
-            @Override
-            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
-                onMovieResponseReceived(response);
-            }
-
-            @Override
-            public void onFailure(Call<MovieResponse> call, Throwable t) {
-                Log.e(LOG_TAG, t.getMessage());
-
-            }
-        });
-    }
-
-    // Method for receiving a Movie response object and displaying its data
-    private void onMovieResponseReceived(Response<MovieResponse> response) {
-        movieAdapter.clear();
-
-        if (response.isSuccessful()) {
-
-            MovieResponse movieResponse = response.body();
-            List<Movie> movies = movieResponse.getMovieResults();
-            movieAdapter.addAll(movies);
-
-            gridView.setVisibility(View.VISIBLE);
-            errorMessage.setVisibility(View.GONE);
-
-        } else {
-            errorMessage.setVisibility(View.VISIBLE);
-            gridView.setVisibility(View.GONE);
-            Log.e(LOG_TAG, "Code: " + response.code());
-
-        }
-    }
 
 }
