467b76d22dd523e2010b8233f022e98fc2185853
Daniel Cronin <dcronin202@gmail.com>
2019-08-28 16:19:54 -0500

    Created a MovieExecutors class

---
+++ app/src/main/java/com/example/android/popularmovies/database/MovieExecutors.java
@@ -0,0 +1,59 @@
+package com.example.android.popularmovies.database;
+
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import android.os.Handler;
+
+
+public class MovieExecutors {
+
+    // Singleton instantiation
+    private static final Object LOCK = new Object();
+    private static MovieExecutors sInstance;
+    private final Executor diskIO;
+    private final Executor mainThread;
+    private final Executor networkIO;
+
+    public MovieExecutors(Executor diskIO, Executor mainThread, Executor networkIO) {
+        this.diskIO = diskIO;
+        this.mainThread = mainThread;
+        this.networkIO = networkIO;
+    }
+
+    public static MovieExecutors getInstance() {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = new MovieExecutors(Executors.newSingleThreadExecutor(),
+                        Executors.newFixedThreadPool(3),
+                        new MainThreadExecutor());
+            }
+        }
+        return sInstance;
+    }
+
+    public Executor diskIO() {
+        return diskIO;
+    }
+
+    public Executor mainThread() {
+        return mainThread;
+    }
+
+    public Executor networkIO() {
+        return networkIO;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHandler.post(command);
+        }
+
+    }
+
+}
