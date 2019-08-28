6e994cfea0b31848de8985aff8259b24335fffbf
Daniel Cronin <dcronin202@gmail.com>
2019-08-28 10:34:27 -0500

    Added methods for getting Popular and Top Rated movies

--- app/src/main/java/com/example/android/popularmovies/database/MovieViewModel.java
+++ app/src/main/java/com/example/android/popularmovies/database/MovieViewModel.java
@@ -3,6 +3,8 @@ package com.example.android.popularmovies.database;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.example.android.popularmovies.data.Movie;
 
@@ -10,28 +12,30 @@ import java.util.List;
 
 public class MovieViewModel extends AndroidViewModel {
 
-    private MovieRepository mRepository;
+    private static final String LOG_TAG = MovieViewModel.class.getSimpleName();
 
-    private LiveData<List<Movie>> mAllMovies;
+    private MovieRepository repository;
+    private LiveData<List<Movie>> movies;
 
 
-    public MovieViewModel(Application application) {
+    public MovieViewModel(@NonNull Application application) {
         super(application);
-        mRepository = new MovieRepository(application);
-        mAllMovies = mRepository.getAllMovies();
+        repository = new MovieRepository(application);
+        Log.d(LOG_TAG, "Actively retrieving the movies from the database.");
+        movies = repository.getAllMovies();
 
     }
 
-    public LiveData<List<Movie>> getAllMovies() {
-
-        // May need to call movies from Retrofit here
-        //
-
-        return mAllMovies;
+    public LiveData<List<Movie>> getMovies() {
+        return repository.getAllMovies();
     }
 
-    public void insert(Movie movie) {
-        mRepository.insert(movie);
+    public void getPopularMovies() {
+        repository.callPopularMovies();
+    }
+
+    public void getTopRatedMovies() {
+        repository.callTopRatedMovies();
     }
 
 }
