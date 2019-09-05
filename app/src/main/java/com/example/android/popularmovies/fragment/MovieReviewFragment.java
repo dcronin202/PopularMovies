74f0d96f4ea2603ab4a537849f0ce749cc9f57e1
Daniel Cronin <dcronin202@gmail.com>
2019-09-05 14:53:53 -0500

    Added an error message if no video or review data is displayed, and added a divider between the items on the RecyclerView

--- app/src/main/java/com/example/android/popularmovies/fragment/MovieReviewFragment.java
+++ app/src/main/java/com/example/android/popularmovies/fragment/MovieReviewFragment.java
@@ -2,17 +2,22 @@ package com.example.android.popularmovies.fragment;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.android.popularmovies.R;
 import com.example.android.popularmovies.adapter.ReviewsRecyclerViewAdapter;
+import com.example.android.popularmovies.data.Movie;
 import com.example.android.popularmovies.data.MovieReviews;
 import com.example.android.popularmovies.viewmodel.MovieDetailViewModel;
 
@@ -21,6 +26,7 @@ import java.util.ArrayList;
 public class MovieReviewFragment extends Fragment {
 
     private static final String LOG_TAG = MovieReviewFragment.class.getSimpleName();
+    private TextView errorMessage;
 
     private MovieDetailViewModel viewModel;
 
@@ -30,7 +36,7 @@ public class MovieReviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.movie_review_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_movie_review, container, false);
 
         reviewAdapter = new ReviewsRecyclerViewAdapter(getActivity(), new ArrayList<MovieReviews>());
         recyclerView = view.findViewById(R.id.recyclerview_reviews);
@@ -38,7 +44,13 @@ public class MovieReviewFragment extends Fragment {
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        // Divider between Reviews in RecyclerView
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+        itemDecoration.setDrawable(ContextCompat.getDrawable(getContext(), R.drawable.divider));
+        recyclerView.addItemDecoration(itemDecoration);
+
         setupReviewsViewModel();
+        setupErrorMessage(view);
 
         return view;
 
@@ -47,12 +59,35 @@ public class MovieReviewFragment extends Fragment {
     private void setupReviewsViewModel() {
         viewModel = ViewModelProviders.of(getActivity()).get(MovieDetailViewModel.class);
         viewModel.getReviews().observe(this, new Observer<ArrayList<MovieReviews>>() {
+
             @Override
             public void onChanged(@Nullable ArrayList<MovieReviews> movieReviews) {
-                reviewAdapter.updateReviewList(movieReviews);
+
+                if (movieReviews.size() > 0) {
+                    reviewAdapter.updateReviewList(movieReviews);
+                    recyclerView.setVisibility(View.VISIBLE);
+                    errorMessage.setVisibility(View.GONE);
+
+                } else {
+                    recyclerView.setVisibility(View.GONE);
+                    errorMessage.setVisibility(View.VISIBLE);
+                }
             }
         });
+
         viewModel.getReviewList();
+    }
+
+    private void setupErrorMessage(View view) {
+        final Movie movie = viewModel.getMovieDetails();
+
+        Resources resources = getResources();
+        String movieTitle = movie.getMovieTitle();
+        String errorText = resources.getString(R.string.no_reviews_available, movieTitle);
+
+        errorMessage = view.findViewById(R.id.reviews_error_message);
+        errorMessage.setText(errorText);
+
     }
 
 }
--- app/src/main/java/com/example/android/popularmovies/fragment/MovieVideoFragment.java
+++ app/src/main/java/com/example/android/popularmovies/fragment/MovieVideoFragment.java
@@ -2,17 +2,22 @@ package com.example.android.popularmovies.fragment;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.android.popularmovies.R;
 import com.example.android.popularmovies.adapter.VideosRecyclerViewAdapter;
+import com.example.android.popularmovies.data.Movie;
 import com.example.android.popularmovies.data.MovieVideos;
 import com.example.android.popularmovies.viewmodel.MovieDetailViewModel;
 
@@ -21,16 +26,18 @@ import java.util.ArrayList;
 public class MovieVideoFragment extends Fragment {
 
     private static final String LOG_TAG = MovieVideoFragment.class.getSimpleName();
+    private TextView errorMessage;
 
     private MovieDetailViewModel viewModel;
 
     private RecyclerView recyclerView;
     private VideosRecyclerViewAdapter videoAdapter;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.movie_video_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_movie_video, container, false);
 
         videoAdapter = new VideosRecyclerViewAdapter(getActivity(), new ArrayList<MovieVideos>());
         recyclerView = view.findViewById(R.id.recyclerview_videos);
@@ -38,7 +45,13 @@ public class MovieVideoFragment extends Fragment {
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        // Divider between Videos in RecyclerView
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+        itemDecoration.setDrawable(ContextCompat.getDrawable(getContext(), R.drawable.divider));
+        recyclerView.addItemDecoration(itemDecoration);
+
         setupVideosViewModel();
+        setupErrorMessage(view);
 
         return view;
 
@@ -47,12 +60,35 @@ public class MovieVideoFragment extends Fragment {
     private void setupVideosViewModel() {
         viewModel = ViewModelProviders.of(getActivity()).get(MovieDetailViewModel.class);
         viewModel.getVideos().observe(this, new Observer<ArrayList<MovieVideos>>() {
+
             @Override
             public void onChanged(@Nullable ArrayList<MovieVideos> movieVideos) {
-                videoAdapter.updateVideoList(movieVideos);
+
+                if (movieVideos.size() > 0) {
+                    videoAdapter.updateVideoList(movieVideos);
+                    recyclerView.setVisibility(View.VISIBLE);
+                    errorMessage.setVisibility(View.GONE);
+
+                } else {
+                    recyclerView.setVisibility(View.GONE);
+                    errorMessage.setVisibility(View.VISIBLE);
+                }
             }
         });
+
         viewModel.getVideoList();
+    }
+
+    private void setupErrorMessage(View view) {
+        final Movie movie = viewModel.getMovieDetails();
+
+        Resources resources = getResources();
+        String movieTitle = movie.getMovieTitle();
+        String errorText = resources.getString(R.string.no_videos_available, movieTitle);
+
+        errorMessage = view.findViewById(R.id.video_error_message);
+        errorMessage.setText(errorText);
+
     }
 
 }
